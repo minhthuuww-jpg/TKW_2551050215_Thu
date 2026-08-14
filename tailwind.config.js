38f0f08ec33f1ba9414347c1ed7e78e483e9e0bc
2551050215thu-tech <2551050215thu@ou.edu.vn>
2026-08-14 22:35:03 +0700

    Thêm cấu trúc HTML và Tailwind config cho trang thời trang

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    ".D:\\TH Thiết kế Web\\index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: '#fdf2f8',
+          100: '#fce7f3',
+          200: '#fbcfe8',
+          300: '#f9a8d4',
+          400: '#f472b6',
+          500: '#ec4899',
+          600: '#db2777',
+          700: '#be185d',
+          800: '#9d174d',
+          900: '#831843',
+        }
+      },
+      fontFamily: {
+        display: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
